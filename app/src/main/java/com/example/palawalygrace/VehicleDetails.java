7e88e7fb99a4f9cfa86f2103e9abfdf313bb5557
nana hamzah <suehana_96@yahoo.com>
2019-07-12 22:38:05 +0800

    incomplete vehicle details ui

---
+++ app/src/main/java/com/example/palawalygrace/VehicleDetails.java
@@ -0,0 +1,17 @@
+package com.example.palawalygrace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class VehicleDetails extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.vehicle_details );
+
+    }
+
+}
